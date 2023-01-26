babbdde5f10f6b0352613ceb31c2627e4699743e
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2023-01-26 21:18:27 +0100

    update project configs

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  clientId: '0168452a79a84bbebaa85410b186b9bc',
+  redirectUrl: 'http://localhost:5200/callback.html'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
