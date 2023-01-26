2cf2458994999d8dae63bc9f1d85e87a55013cd4
Anatolii Pisarivskyi <pisarivskii@gmail.com>
2023-01-26 16:39:24 +0100

    add needed dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <router-outlet></router-outlet>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'flex flex-column h-full flex-grow-1'
+  }
 })
-export class AppComponent {
-  title = 'guessify';
-}
+export class AppComponent {}
